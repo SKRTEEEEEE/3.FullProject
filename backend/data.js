860568fe3d732cb0c7e0a6480bbc04e89d51b92c
Adan <adanreh.m@gmail.com>
2023-03-02 19:41:45 +0100

    Lesson12-Complet-Add-Cart

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '2',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -25,6 +27,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '3',
       name: 'Adidas Slim Shirt',
       slug: 'adidas-slim-shirt',
       category: 'Shirts',
@@ -37,6 +40,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
       name: 'Adidas Slim Pant',
       slug: 'adidas-slim-pant',
       category: 'pants',
